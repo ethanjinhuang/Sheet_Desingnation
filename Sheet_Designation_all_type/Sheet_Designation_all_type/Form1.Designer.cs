106544b613d0f1c87b3db61de89aa4b308d299c6
GimHuang <55234358+GimHuang@users.noreply.github.com>
2020-05-11 19:33:19 +0800

    Add files via upload

---
+++ Sheet_Designation_all_type/Sheet_Designation_all_type/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Sheet_Designation_all_type
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(708, 285);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(119, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(889, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 85);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Source Sans Pro Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(782, 430);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 50);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 3;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(707, 314);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            39375,
+            0,
+            0,
+            196608});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 3;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Location = new System.Drawing.Point(708, 345);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            180,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            115562,
+            0,
+            0,
+            196608});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(579, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "latitude";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(579, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "longitude";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(573, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(480, 232);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(576, 436);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 34);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "图幅编号:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(579, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "plotting scale";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 601);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Sheet_Designation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Sheet_Designation_all_type/Sheet_Designation_all_type/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sheet_Designation_all_type
+{
+    public partial class Form1 : Form
+    {
+        Graphics grid_box;
+        Graphics half_box;
+        //Origion 
+        double delta_lambda = 0;
+        double delta_phi = 0;
+        int grid_number = 0;
+        char plot_ID = 'A';
+        int y_in_million = 0;
+        int x_in_million = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void caculate_number(double latitude,double longitude)
+        {
+            //cauculate 1:100w
+            int a = (int)Math.Floor(latitude / 4) + 1;
+            int b = (int)Math.Floor(longitude / 6) + 31;
+            int c = (int)(4 / delta_phi) - (int)Math.Floor(latitude % 4 / delta_phi);
+            int d = (int)Math.Floor((longitude % 6) / delta_lambda) + 1;
+            y_in_million = a;
+            x_in_million = b-31;
+            label1.Text = (char)(a + 64) + b.ToString().PadLeft(2, '0') + plot_ID + c.ToString().PadLeft(3, '0') + d.ToString().PadLeft(3, '0');
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            //load plotting scale
+            comboBox1.Items.Add("1:50w");
+            comboBox1.Items.Add("1:25w");
+            comboBox1.Items.Add("1:10w");
+            comboBox1.Items.Add("1:5w");
+
+
+
+
+            //draw the half box
+            Pen penline = new Pen(Color.Blue);
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox2.Focus();
+            half_box = pictureBox2.CreateGraphics();
+            Bitmap new1 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            Graphics gpn = Graphics.FromImage(new1);
+
+            Pen pointpen = new Pen(Color.Green, 2);
+            float basic_gap = pictureBox2.Height / 22;
+
+            for (int i = 0; i < 22; i++)
+            {
+                gpn.DrawPie(penline, pictureBox2.Width / 2 - (i + 1) * basic_gap, -(i + 1) * basic_gap, (i + 1) * basic_gap * 2, (i + 1) * basic_gap * 2, 0, 180);
+            }
+
+            for (int i = 0; i < 180; i += 6)
+            {
+                gpn.DrawPie(penline, pictureBox2.Width / 2 - 22 * basic_gap, -22 * basic_gap, 22 * basic_gap * 2, 22 * basic_gap * 2, i, 180);
+            }
+            pictureBox2.BackgroundImage = new1;
+            pictureBox2.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //eco
+            int grid_box_width = pictureBox1.Width;
+            int grid_box_height = pictureBox1.Height;
+
+
+            //Judge the Selected
+            if (delta_lambda==0 && delta_phi==0 && grid_number==0)
+            {
+                MessageBox.Show("Please selected plotting scale");
+            }else
+            {
+                // Draw the grid Box
+                pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+                pictureBox1.Focus();
+                grid_box = pictureBox1.CreateGraphics();
+                Bitmap pic1 = new Bitmap(grid_box_width, grid_box_height);
+                Graphics gb = Graphics.FromImage(pic1);
+                Pen penline = new Pen(Color.Blue);
+                Font font1 = new Font("Cambria Math", 200 / grid_number, FontStyle.Regular);
+
+                for (int i=0;i <= (grid_number + 1); i++)     //绘制竖线
+                {
+                    gb.DrawLine(penline, (i + 1) * grid_box_width / (grid_number + 1), 0, (i + 1) * grid_box_width / (grid_number + 1), grid_box_height);
+                    gb.DrawString(Convert.ToString(i), font1, Brushes.Black, i * grid_box_width / (grid_number + 1), 0);
+                }
+                for (int i = 0; i <= (grid_number + 1); i++)      //绘制横线
+                {
+                    gb.DrawLine(penline, 0, (i + 1) * grid_box_height / (grid_number + 1), grid_box_width, (i + 1) * grid_box_height / (grid_number + 1));
+                    gb.DrawString(Convert.ToString(i), font1, Brushes.Black, 0, i * grid_box_height / (grid_number + 1));
+                }
+
+
+
+
+                caculate_number((double)this.numericUpDown1.Value, (double)this.numericUpDown2.Value);
+
+                double x = Math.Abs((double)this.numericUpDown1.Value - y_in_million * 4) / 4 * (pictureBox1.Width - pictureBox1.Width / (grid_number + 1)) + pictureBox1.Width / (grid_number + 1);
+                double y = Math.Abs((double)this.numericUpDown2.Value - x_in_million * 6) / 6 * (pictureBox1.Height-pictureBox1.Height/(grid_number+1))+ pictureBox1.Height / (grid_number + 1);
+
+                Pen penpoint = new Pen(Color.Red, 2);
+                gb.DrawRectangle(penpoint, (float)x, (float)y, 2, 2);
+
+                //刷线图像
+                pictureBox1.BackgroundImage = pic1;
+                pictureBox1.Refresh();              
+
+
+
+
+
+
+
+                //Draw the 100w 
+                pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+                pictureBox2.Focus();
+                half_box = pictureBox2.CreateGraphics();
+                Bitmap new1 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                Graphics gpn = Graphics.FromImage(new1);
+
+                Pen pointpen = new Pen(Color.Green, 2);
+                Pen redpen = new Pen(Color.Red, 2);
+                float basic_gap = pictureBox2.Height / 22;
+
+                for (int i = 0; i < 22; i++)
+                {
+                    gpn.DrawPie(penline, pictureBox2.Width / 2 - (i + 1) * basic_gap, -(i + 1) * basic_gap, (i + 1) * basic_gap * 2, (i + 1) * basic_gap * 2, 0, 180);
+                }
+
+                for (int i = 0; i < 180; i += 6)
+                {
+                    gpn.DrawPie(penline, pictureBox2.Width / 2 - 22 * basic_gap, -22 * basic_gap, 22 * basic_gap * 2, 22 * basic_gap * 2, i, 180);
+                }
+                gpn.DrawPie(redpen, pictureBox2.Width / 2 - (22 - y_in_million + 1) * basic_gap, -(22 - y_in_million + 1) * basic_gap, (22 - y_in_million + 1) * basic_gap * 2, (22 - y_in_million + 1) * basic_gap * 2, 180 - x_in_million * 6, 6);
+                gpn.DrawPie(pointpen, pictureBox2.Width / 2 - (22 - y_in_million) * basic_gap, -(22 - y_in_million) * basic_gap, (22 - y_in_million) * basic_gap * 2, (22 - y_in_million) * basic_gap * 2, 180 - x_in_million * 6, 6);
+               
+
+                pictureBox2.BackgroundImage = new1;
+                pictureBox2.Refresh();
+            }
+                
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "1:50w":
+                    delta_lambda = 3; delta_phi = 2; grid_number = 2; plot_ID = 'B'; break;
+                case "1:25w":
+                    delta_lambda = 1.5; delta_phi = 1; grid_number = 4; plot_ID = 'C'; break;
+                case "1:10w":
+                    delta_lambda = 3 / 6.0; delta_phi = 20 / 60.0; grid_number = 12; plot_ID = 'D'; break;
+                case "1:5w":
+                    delta_lambda = 15 / 60.0; delta_phi = 10 / 60.0; grid_number = 24; plot_ID = 'E'; break;
+                default:
+                    MessageBox.Show("Please selected plotting scale"); break;
+            }
+            if (delta_lambda == 0 && delta_phi == 0 && grid_number == 0)
+            {
+                MessageBox.Show("Please selected plotting scale");
+            }
+            else
+            {
+                int grid_box_width = pictureBox1.Width;
+                int grid_box_height = pictureBox1.Height;
+                pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+                pictureBox1.Focus();
+                grid_box = pictureBox1.CreateGraphics();
+                Bitmap pic1 = new Bitmap(grid_box_width, grid_box_height);
+                Graphics gb = Graphics.FromImage(pic1);
+                Pen penline = new Pen(Color.Blue);
+                Font font1 = new Font("Cambria Math", 200 / grid_number, FontStyle.Regular);
+
+                for (int i = 0; i <= (grid_number + 1); i++)     //axis-y
+                {
+                    gb.DrawLine(penline, (i + 1) * grid_box_width / (grid_number + 1), 0, (i + 1) * grid_box_width / (grid_number + 1), grid_box_height);
+                    gb.DrawString(Convert.ToString(i), font1, Brushes.Black, i * grid_box_width / (grid_number + 1), 0);
+                }
+                for (int i = 0; i <= (grid_number + 1); i++)      //axis-x
+                {
+                    gb.DrawLine(penline, 0, (i + 1) * grid_box_height / (grid_number + 1), grid_box_width, (i + 1) * grid_box_height / (grid_number + 1));
+                    gb.DrawString(Convert.ToString(i), font1, Brushes.Black, 0, i * grid_box_height / (grid_number + 1));
+                }
+                pictureBox1.BackgroundImage = pic1;
+                pictureBox1.Refresh();
+            }
+
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            button1_Click(null,null);
+        }
+    }
+}
